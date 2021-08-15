82c76e6a791163f118a20ebc92742246634bd585
Micheal Skinner <michealskinner@Micheals-MacBook-Pro.local>
2021-08-14 22:29:35 -0400

    started backend express / mock backend

---
+++ backend/mockDb/blogPosts.js
@@ -0,0 +1,34 @@
+module.exports.blogPost = [
+  {
+    id: 1, // user id connection
+    postId: 1,
+    title: "Mercy",
+    content: "asdfasdfasfd",
+    content1: "adfghdfghdfh",
+    content2: "zxcvzxcvzxcv",
+  },
+  {
+    id: 2, // user id connection
+    postId: 1,
+    title: "Peace",
+    content: "asdfasdfasfd",
+    content1: "adfghdfghdfh",
+    content2: "zxcvzxcvzxcv",
+  },
+  {
+    id: 3, // user id connection
+    postId: 2,
+    title: "Joy",
+    content: "asdfasdfasfd",
+    content1: "adfghdfghdfh",
+    content2: "zxcvzxcvzxcv",
+  },
+  {
+    id: 4, // user id connection
+    postId: 5,
+    title: "Humility",
+    content: "asdfasdfasfd",
+    content1: "adfghdfghdfh",
+    content2: "zxcvzxcvzxcv",
+  },
+];
---
+++ backend/mockDb/users.js
@@ -0,0 +1,27 @@
+module.exports.user = [
+    {
+      id: 1,
+      username: "Tom",
+      password: "string"
+    },
+    {
+      id: 2,
+      username: "Dick",
+      password: "string"
+    },
+    {
+      id: 3,
+      username: "Harry",
+      password: "string"
+    },
+    {
+      id: 4,
+      username: "Lucy",
+      password: "string"
+    },
+    {
+      id: 5,
+      username: "Lue",
+      password: "string"
+    },
+  ];
